f482a07cfeaaff8ae28b8cd03090c60f67fe8de5
yohannesm <jonmersha@gmail.com>
2022-06-03 17:36:17 +0300

    2nd commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+const path = require('path');
+app.use(
+    express.urlencoded({
+      extended: true
+    })
+  )
+ 
+
+  app.use(express.json())
+  app.use('/', express.static(path.join(__dirname, 'public')))
+
+
+
+
+  app.get('/index',(req,res)=>{
+    res.send("the page is working")
+})
+
+
+app.listen(3000)
